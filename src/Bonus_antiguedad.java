42222f47b2051ce75207098baf741e188f65ec54
David Tabarez <davicitotabarez@gmail.com>
2022-11-18 13:15:44 -0600

    Se agrego un switch para una mejor seleccion de pago, y se completo el if else de antiguedad en el CASE 1

--- src/Bonus_antiguedad.java
+++ src/Bonus_antiguedad.java
@@ -10,21 +10,45 @@ public class Bonus_antiguedad {
 		double sueldo_bono = 0;
 		double bono_sueldo = 0;
 		double total = 0;
+		byte opcion = 0;
 		
-		System.out.println("Ingrese su años trabajando: ");
-		antiguedad = entrada.nextByte();
+		System.out.println("Seleccione su tipo de pago");
+		System.out.println("Pago por antiguedad (1): ");
+		System.out.println("Pago por sueldo     (2):");
+		opcion = entrada.nextByte();
 		
-		if (antiguedad <= 1) {
-			System.out.println("no cuentas con antiguedad");
+		switch (opcion) {
+		case 1:
 			
-		} else if (antiguedad >= 2 && antiguedad < 5 ) {
-			System.out.println("20%");
+			System.out.println("Ingrese su años trabajando: ");
+			antiguedad = entrada.nextByte();
 			
-		} else {
-			System.out.println("30%");
+			System.out.println("ingrese su sueldo: ");
+			sueldo = entrada.nextDouble();
+			if (antiguedad <= 1) {
+				System.out.println("no cuentas con antiguedad");
+				
+			} else if (antiguedad >= 2 && antiguedad < 5 ) {
+				System.out.println("20%");
+				bono_antiguedad = (sueldo * 0.20);
+				total = (sueldo + bono_antiguedad);
+				
+			} else {
+				System.out.println("30%");
+				bono_antiguedad = (sueldo * 0.30);
+				total = (sueldo + bono_antiguedad);
+				
+			} // fin if else
 			
-		} // fin if else
+			System.out.println("Tu pago final es de: " + total);
 			
+			break;
+		case 2: 
+
+		default:
+			break;
+		}
+		
 		
 
 	}
