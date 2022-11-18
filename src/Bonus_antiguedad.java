4da9df267cacf39856b113187fb2379cf82f2c98
David Tabarez <davicitotabarez@gmail.com>
2022-11-18 13:00:45 -0600

    Se termina con el primer condicional if else (antiguedad)

--- src/Bonus_antiguedad.java
+++ src/Bonus_antiguedad.java
@@ -6,12 +6,26 @@ public class Bonus_antiguedad {
 		Scanner entrada = new Scanner(System.in);
 		
 		byte antiguedad = 0;
-		double sueldo = 0;
+		double sueldo = 0 , bono_antiguedad = 0;
+		double sueldo_bono = 0;
+		double bono_sueldo = 0;
 		double total = 0;
 		
-		if (condition) {
+		System.out.println("Ingrese su años trabajando: ");
+		antiguedad = entrada.nextByte();
+		
+		if (antiguedad <= 1) {
+			System.out.println("no cuentas con antiguedad");
 			
-		}
+		} else if (antiguedad >= 2 && antiguedad < 5 ) {
+			System.out.println("20%");
+			
+		} else {
+			System.out.println("30%");
+			
+		} // fin if else
+			
+		
 
 	}
 
